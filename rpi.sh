59d71055f3e3063b3de53b3fe61d42b281539454
elaouby <95683881+elaouby@users.noreply.github.com>
2023-11-03 20:59:23 +0300

    Update rpi.sh

--- rpi.sh
+++ rpi.sh
@@ -1,4 +1,4 @@
-#git clone https://elaouby@github.com/elaouby/RPI /home/admin/RPI ; cd /home/admin/RPI ; sudo bash ./rpi.sh -r -p -d -g
+#git clone https://elaouby@github.com/elaouby/RPI /home/admin/RPI ; cd /home/admin/RPI ; sudo bash ./rpi.sh -p -r -d -g
 #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
 while getopts "rpdg" option; do
 case $option in
